6989dd6c09e1e4963de70d49e58fbfcc314b730a
ajaysagarn <ajaysagarn@gmail.com>
2022-04-07 21:43:53 -0500

    Add alumni search page

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { Router } from "@reach/router"
 import React from "react"
 import HomePage from './components/AppFrame/HomePage'
+import Alumnisearch from "./components/search/AlumniSearch"
 
 
 
@@ -8,6 +9,7 @@ export default function MyRouter(){
     return (
        <Router>
         <HomePage path="/" />
+        <Alumnisearch path="/search" />
       </Router>
     )
 }
