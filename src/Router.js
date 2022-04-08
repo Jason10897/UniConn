897775fab787b452092ab32f01d15bb6148ed2e3
ajaysagarn <ajaysagarn@gmail.com>
2022-04-07 21:50:29 -0500

    Add user profile and roadmap pages

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,8 @@
 import { Router } from "@reach/router"
 import React from "react"
 import HomePage from './components/AppFrame/HomePage'
+import UserProfile from "./components/profile/UserProfile"
+import RoadMap from "./components/roadmap/Roadmap"
 import Alumnisearch from "./components/search/AlumniSearch"
 
 
@@ -10,6 +12,8 @@ export default function MyRouter(){
        <Router>
         <HomePage path="/" />
         <Alumnisearch path="/search" />
+        <UserProfile path="/profile" />
+        <RoadMap path="/roadmap"/>
       </Router>
     )
 }
---
+++ src/components/roadmap/Roadmap.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function RoadMap(){
+    return (
+        <div>
+            This is the roadmap page
+        </div>
+    )
+}
