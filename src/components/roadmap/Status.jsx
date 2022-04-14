0bac7cdfde3a4c9b6eb4c346ce7714dbaf748302
Jason10897 <jasondsouza10897@gmail.com>
2022-04-14 04:37:35 -0500

    Enhance Status text

--- src/components/roadmap/Status.jsx
+++ src/components/roadmap/Status.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Status () {
     return (
         <React.Fragment>
-            <p style = {{color: 'green', textAlign: 'center'}}>Completed</p>
+            <p style = {{color: "green", fontWeight: "bold", textAlign: "center"}}>Completed</p>
         </React.Fragment>
     );
 }
