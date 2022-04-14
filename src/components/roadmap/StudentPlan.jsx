0fa7ac8ac88fa5d0091d1fa0ae78e236fbbf98b2
Jason10897 <jasondsouza10897@gmail.com>
2022-04-13 19:34:49 -0500

    Add Next Step text field for Student user

--- src/components/roadmap/StudentPlan.jsx
+++ src/components/roadmap/StudentPlan.jsx
@@ -5,7 +5,11 @@ import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import CircleIcon from "@mui/icons-material/Circle";
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 import { styled } from "@mui/material/styles";
+import { textAlign } from "@mui/system";
 
 const StyledListPad = styled(ListItemIcon)(({ theme }) => ({
     minWidth: "25px"
@@ -14,6 +18,11 @@ const StyledListPad = styled(ListItemIcon)(({ theme }) => ({
 const Bullet = styled(CircleIcon)(({ theme }) => ({
     fontSize: "10px",
     color: "black"
+  }));
+
+const SendButton = styled(Button)(({ theme }) => ({
+    minWidth: "10px",
+    maxWidth: "40px",
   }));
 
 const plan = [{id: 1, text: "Step 1"}, {id: 2, text: "Step 2"}, {id: 3, text: "Step 3"}];
@@ -30,6 +39,10 @@ export default function StudentPlan () {
                                 <ListItemText primary={item.text}></ListItemText>
                             </ListItem>})}
                 </List>
+                <TextField id="outlined-basic" label="Next Step" variant="outlined" 
+                InputProps={{endAdornment:  <SendButton size="small" variant="contained" 
+                                                endIcon={<SendIcon sx = {{marginLeft: "-10px"}}/>}>
+                                            </SendButton>}}/>
             </Box>
     );
 }
