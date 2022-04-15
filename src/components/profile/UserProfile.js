23325bc57bb8ecdf2b422d1d4774caace1bb9f38
Harsh <harsh.jethwani16@gmail.com>
2022-04-15 17:24:31 -0500

    CreateUserProfiles

--- src/components/profile/UserProfile.js
+++ src/components/profile/UserProfile.js
@@ -1,9 +1,51 @@
 import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { alpha, styled } from "@mui/material/styles";
+import PersonIcon from "@mui/icons-material/Person";
+import Typography from "@mui/material/Typography";
+import ChatIcon from "@mui/icons-material/Chat";
+import { Button } from "@mui/material";
+import { Stack } from "@mui/material";
+import { Divider } from '@mui/material';
 
-export default function UserProfile(){
-    return (
-        <div>
-            This is the user profile page
-        </div>
-    )
+const styleProfileIcon = {
+  width: "96vw",
+  height: "20vh",
+};
+
+export default function UserProfile() {
+  return (
+    <>
+      <AccountCircleIcon style={styleProfileIcon} /> {/* Profile Icon */}
+      <Typography variant="h4" gutterBottom component="div">
+        Name
+      </Typography>
+      <Typography variant="subtitle1" gutterBottom component="div">
+        Bachelors in Computer Science
+      </Typography>
+      {/* Buttons */}
+      <Stack
+        spacing={2}
+        direction="row"
+        sx={{
+          alignItems: "center",
+          alignContent: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Button variant="contained">Connect</Button>
+
+        <Button variant="contained" color="success">
+          Recommend
+        </Button>
+
+        <ChatIcon />
+      </Stack>
+      <div>
+      <Divider sx={{
+          
+      }} />
+      </div>
+    </>
+  );
 }
