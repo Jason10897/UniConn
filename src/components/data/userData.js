4c3775a4a4602348bf06e9d2c5298516fcf96c74
ajaysagarn <ajaysagarn@gmail.com>
2022-04-18 16:38:21 -0500

    Add User data

---
+++ src/components/data/userData.js
@@ -0,0 +1,215 @@
+export const userTypes = {
+    ALUMNI: "ALUMNI",
+    STUDENT: "STUDENT"
+}
+
+export const departments = {
+    CSE: "Computer Science",
+    BUS: "Business",
+    ENG: "Engineering",
+    MED: "Medicine",
+    LAW: "Law",
+    OTH: "Others"
+}
+
+export const Companies = {
+    Google: "Google",
+    Microsoft: "Microsoft",
+    Amazon: "Amazon",
+    Apple: "Apple",
+    PayPal: "PayPal"
+}
+
+export const Locations = {
+    NYC: "New York City",
+    CHI: "Chicago",
+    STL: "Seattle",
+    AUS: "Austin",
+    SF: "San Francisco"
+}
+
+
+export const AlumniUsers = [
+    {
+        firstName: "Jane",
+        lastName: "Doe",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Lovell ",
+        lastName: "Lyanna",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: true
+    },
+    {
+        firstName: "Alanna",
+        lastName: "Branda",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Hugo",
+        lastName: "Rudyard",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: true
+    },
+    {
+        firstName: "Emmitt",
+        lastName: "Christopher",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Alfred",
+        lastName: "Odin",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Arlie",
+        lastName: "Brooks",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Tara",
+        lastName: "Calanthe",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: true
+    },
+    {
+        firstName: "Kiera",
+        lastName: "Ashlie",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Melva",
+        lastName: "Malene",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Leta",
+        lastName: "Ever",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: true
+    },
+    {
+        firstName: "Juliana",
+        lastName: "Karlene",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Skylar",
+        lastName: "white",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Walter",
+        lastName: "White",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Mike",
+        lastName: "Schrader",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Jesse",
+        lastName: "Pinkman",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Saul",
+        lastName: "Goodman",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Gustavo",
+        lastName: "Fring",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Paulette",
+        lastName: "Kriemhild",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: false
+    },
+    {
+        firstName: "Aaliyah",
+        lastName: "Emberly",
+        type: userTypes.ALUMNI,
+        domain: departments.CSE,
+        company: Companies.Google,
+        location: Locations.NYC,
+        isConnected: true
+    }
+
+]
+
