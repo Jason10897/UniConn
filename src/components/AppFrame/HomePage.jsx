eda86c5c42721a453664ce51eeadc34adc987f39
Harsh <harsh.jethwani16@gmail.com>
2022-04-19 22:10:15 -0500

    Connect button added in the home page

--- src/components/AppFrame/HomePage.jsx
+++ src/components/AppFrame/HomePage.jsx
@@ -1,6 +1,8 @@
 import Typography from '@mui/material/Typography';
 import * as React from 'react';
 import EventSlider from './EventSlider';
+import { Box } from '@mui/material';
+import { Button } from '@mui/material';
 
 export default function HomePage(){
     return (
@@ -17,7 +19,17 @@ export default function HomePage(){
         {/* <Typography variant="h5" align="center" color="text.secondary" component="p">
         Add University Events Slider Here
         </Typography> */}
+        <Box>
+        <Button variant="contained" sx={{
+          marginBottom:2,
+          maxWidth: "250px",
+          maxHeight: "200px",
+          minWidth: "150px",
+          minHeight: "150px"
+        }}>Search and Connect <br /> with <br /> Alumni/Students</Button>
         <EventSlider/>
+        </Box>
+        
         </React.Fragment>
     )
 }
