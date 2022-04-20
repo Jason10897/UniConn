a5a9c372121483c9d8dd1b040d6d26b41c693383
Harsh <harsh.jethwani16@gmail.com>
2022-04-19 23:21:28 -0500

    HomePage button and slider css done

--- src/components/AppFrame/HomePage.jsx
+++ src/components/AppFrame/HomePage.jsx
@@ -4,32 +4,41 @@ import EventSlider from './EventSlider';
 import { Box } from '@mui/material';
 import { Button } from '@mui/material';
 
+
 export default function HomePage(){
     return (
         <React.Fragment>
         <Typography
           component="h1"
           variant="h2"
-          align="center"
+          align="left"
           color="text.primary"
           gutterBottom
         >
           UniConn
         </Typography>
+        <Typography
+          component="h2"
+          variant="h4"
+          align="left"
+          color="text.primary"
+          width={800}
+          gutterBottom
+        >
+          Get the world's most loved alumni engagement platform and build a thriving alumni community online to engage your alumni digitally. 
+        </Typography>
+        
         {/* <Typography variant="h5" align="center" color="text.secondary" component="p">
         Add University Events Slider Here
         </Typography> */}
-        <Box>
+        
         <Button variant="contained" sx={{
           marginBottom:2,
-          maxWidth: "250px",
-          maxHeight: "200px",
-          minWidth: "150px",
-          minHeight: "150px"
-        }}>Search and Connect <br /> with <br /> Alumni/Students</Button>
+          backgroundColor:"green",
+          
+        }}>Search and Connect with Alumni/Students</Button>
+
         <EventSlider/>
-        </Box>
-        
         </React.Fragment>
     )
 }
