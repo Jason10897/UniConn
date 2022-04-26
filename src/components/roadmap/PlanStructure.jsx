aed1be4ee86237d9c03ab8a9265a8993b6acb339
Jason10897 <jasondsouza10897@gmail.com>
2022-04-25 20:25:49 -0500

    Edit alumni feedback content

--- src/components/roadmap/PlanStructure.jsx
+++ src/components/roadmap/PlanStructure.jsx
@@ -25,8 +25,8 @@ export default function PlanStructure() {
       feedback:
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
     },
-    { id: 2, name: "Claudia Alves", feedback: "Feedback 2" },
-    { id: 3, name: "Jane Rodriguez", feedback: "Feedback 3" },
+    { id: 2, name: "Claudia Alves", feedback: "Seems like a great idea!" },
+    { id: 3, name: "Jane Rodriguez", feedback: "You could do study this course better by first taking its prerequisite" },
   ]);
 
   const [openPrompts, handlePrompts] = useState(false);
