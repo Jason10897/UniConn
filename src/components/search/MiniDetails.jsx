40293ce8abd922804e62a42f66686d38a5edf10b
ajaysagarn <ajaysagarn@gmail.com>
2022-05-01 21:27:14 -0500

    Handle request sent change

--- src/components/search/MiniDetails.jsx
+++ src/components/search/MiniDetails.jsx
@@ -1,13 +1,26 @@
-import { Box, Button, Typography } from '@mui/material';
+import { Alert, Box, Button, Snackbar, Typography } from '@mui/material';
 import { navigate } from '@reach/router';
-import React from 'react';
+import React, { useState } from 'react';
 import ConnectButton from './ConnectButton';
+import AddBoxIcon from '@mui/icons-material/AddBox';
 
 export default function MiniDetails({data}){
 
 
     const viewFullProfile = () =>{
         navigate(`/profile/${data?.id}`)
+    }
+
+    const [requestSent, setRequestSent] = useState(!!data?.requestSent);
+    const [snackbarOpen, setOpen] = useState(false)
+
+    const handleSnackClose = () =>{
+        setOpen(false)
+    }
+
+    const showsnackBar = () =>{
+        setRequestSent(true)
+        setOpen(true)
     }
 
     return (
@@ -29,13 +42,18 @@ export default function MiniDetails({data}){
             </Typography>
 
             <Box display='flex' flexDirection='column' sx={{padding: '20px'}}>
-                {(data?.isConnected || data?.requestSent)?
-                <Button variant="contained" sx={{margin:'5px'}} disabled>{data?.requestSent?'Request sent':'Connected'}</Button>
-                :<ConnectButton variant="contained" style={{margin:'5px'}}/>
+                {(data?.isConnected || requestSent)?
+                <Button variant="contained" sx={{margin:'5px'}} disabled>{requestSent?'Request sent':'Connected'}</Button>
+                :<Button onClick={showsnackBar} variant="contained" sx={{margin:'5px'}}>Connect</Button>
                 }
                 <Button variant="contained" sx={{margin:'5px'}} onClick={viewFullProfile}>View full Profile</Button>
             </Box>
-            
+            <Snackbar open={snackbarOpen} anchorOrigin={{ vertical:"bottom", horizontal: "center"}}
+                    autoHideDuration={6000} onClose={handleSnackClose}>
+                    <Alert onClose={handleSnackClose} variant="filled" severity="success" sx={{ width: '100%' }}>
+                        Connection request sent successfully!
+                    </Alert>
+                </Snackbar>  
         </Box>
     )
 }
